72def35b1ee5ff13166b0f3a32fcc13a61731231
PrajyotJadhao15 <prajyotjadhao321@gmail.com>
2026-09-08 14:41:30 +0530

    Update recommendation controller

--- aiservice/src/main/java/com/example/aiservice/controller/RecommendationController.java
+++ aiservice/src/main/java/com/example/aiservice/controller/RecommendationController.java
@@ -6,10 +6,7 @@ import com.example.aiservice.model.Recommendation;
 import com.example.aiservice.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,4 +30,8 @@ public class RecommendationController {
 
         return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
     }
-}
+
+
+    }
+
+
