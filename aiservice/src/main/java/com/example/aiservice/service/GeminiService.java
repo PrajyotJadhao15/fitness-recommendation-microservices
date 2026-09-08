94342997585f776c31ed88b2b2c6602aff100bea
PrajyotJadhao15 <prajyotjadhao321@gmail.com>
2026-09-08 14:42:05 +0530

    Update Gemini service

--- aiservice/src/main/java/com/example/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/example/aiservice/service/GeminiService.java
@@ -8,11 +8,10 @@ import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.ai.chat.client.ChatClient;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
+
 
 import java.util.Map;
 
@@ -21,17 +20,9 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class GeminiService {
 
-    private final WebClient webClient;
     private final AiMetrics aiMetrics;
     private final MeterRegistry meterRegistry;
-
-
-    @Value("${gemini.api.url}")
-    private String Gemini_Url;
-
-    @Value("${gemini.api.key}")
-    private String Gemini_Api_Key;
-
+    private final ChatClient chatClient;
 
 
     @CircuitBreaker(
@@ -40,15 +31,6 @@ public class GeminiService {
     )
         public String getRecommendations(String prompt){
 
-            Map<String, Object> requestBody= Map.of(
-                    "contents", new Object[]{
-                            Map.of("parts", new Object[] {
-
-                                    Map.of("text", prompt)
-                            })
-                    }
-            );
-
         aiMetrics.getAiCalls().increment();
 
         Timer.Sample sample = Timer.start(meterRegistry);
@@ -56,6 +38,8 @@ public class GeminiService {
             try {
 
 
+                //WebClient(Asynchronous Communication)...
+/*
                 String response = webClient.post()
                         .uri(Gemini_Url)
                         .header("Content-Type", "application/json")
@@ -73,16 +57,22 @@ public class GeminiService {
                         )
                         .bodyToMono(String.class)
                         .block();
+*/
 
+                return chatClient
+                        .prompt()
+                        .user(prompt)
+                        .call()
+                        .content();
 
-                return response;
+               // WebClientResponseException
 
-            } catch (WebClientResponseException e) {
+            } catch (GeminiApiException e) {
 
                 log.error("Error while calling Gemini API", e);
 
                 aiMetrics.getAiFailures().increment();
-                throw new GeminiApiException(" Gemini returned HTTP " + e.getStatusCode(), e);
+                throw e;
             }
             catch (Exception ex){
 
@@ -105,11 +95,10 @@ public class GeminiService {
 
         log.error("Gemini API unavailable: {}", ex.getMessage());
 
-        return """
-                {
-                  "message":"AI recommendation currently unavailable"
-                }
-                """;
+        throw new GeminiApiException(
+                "Gemini service temporarily unavailable",
+                ex
+        );
 
 
 
